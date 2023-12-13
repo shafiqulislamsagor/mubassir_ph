bfbd1276485ee26658edaaa628a3669fbb1fa2aa
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-12-14 05:40:17 +0600

    17.5

---
+++ variable-name.js
@@ -0,0 +1,17 @@
+// 1. variable name can not be any keywords
+var false = 96 ;
+var return = true;
+
+// 2. variable name has to be in one work . na space
+var my haome address = "new california";
+
+//3.variable name can not have quotation 
+var "name" = "tamo hanks";
+
+//4.variable name can not starts with a number but can ends with a number
+var 000club = 2252;
+
+//5. How to use long names 
+// can not use dash
+var user-name = "raj bappa";
+
