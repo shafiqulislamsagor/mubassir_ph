3c053fa6fa7967fbe9dc8ba3bc6fc6f444d27e62
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-12-14 00:45:12 +0600

    17.2

---
+++ math.js
@@ -0,0 +1,18 @@
+/**
+ * 1. Number ==> 55,56,-34
+ * 2.Integer: 3,4,5
+ * 3.float: decimal:3.3 , 53.639
+ * 4.operator: addition, subtraction, multiplication
+ * 5.percent,
+ * 6.average
+ * 7.comparison:< , < , == , != , <=
+ * 8.oikik niyom: 2kilo apple and 5 kilo angur:
+ * 9.58,483,38,28,28,83,58,>> average, sum,min,max
+ * 10.series:
+ * EXTRA::::::
+ * 1.Prime
+ * 2.Permutation
+ * 3.Area
+ * ::ALWAYS::
+ * jodi lage sob sikhe fela sathe...
+ */
