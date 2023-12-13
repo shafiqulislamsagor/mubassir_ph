e98c49216f78893c658bba20d7b58dcd9c22d4a3
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-12-14 05:23:23 +0600

    17.4

--- variable.js
+++ variable.js
@@ -1,7 +1,17 @@
 // vary + able = variable
+
+//numeric
 var price = 11 ;
 var age = 17 ;
 var temperature = 38 ;
 var ojon = 45 ;
 var money = 500 ;
-var name = "Shafiqul Islam Sagor"
+
+//
+var name = "Shafiqul Islam Sagor" ;
+var person = "Sodor uddin" ;
+var location = 'Nakla' ;
+
+//Boolean
+var serious = true ;
+var niso = false ;
