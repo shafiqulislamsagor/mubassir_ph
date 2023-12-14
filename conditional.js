e346beee3ac12a66d1726d3ecd9fddef01c66a27
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-12-14 17:53:47 +0600

    18.6

---
+++ conditional.js
@@ -0,0 +1,33 @@
+var iphonePrice = 79000;
+var myBudget = 9500;
+// if iphone price is less than my budget. I will buy the iphone
+/**
+ * if iphone price is less than my budget 
+ *          i will buy the iphone 
+ * 
+ * if (condition) {
+ *      will execute if the condition is true
+ * }
+ */
+
+if (iphonePrice < myBudget) {
+    console.log("i phone kine futani marbo")
+}
+
+var chickenPrice = 500 ;
+var myMoney = 50;
+if (chickenPrice <= myMoney){
+    console.log('Murgir raan khamu and haddi chibamu!!!');
+}
+if (chickenPrice > myMoney) {
+    console.log('Smashed potato and lentils soup');
+}
+
+var price = 50 ;
+var money = 10 ;
+if(price < money){
+    console.log('ami sherpur jabo');
+}
+else{
+    console.log('aj k ar sherpur jabo na');
+}
