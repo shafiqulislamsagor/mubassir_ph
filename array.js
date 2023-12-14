53e4f613c42a34488f910d96abd86ce3f196cb3c
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-12-14 13:14:09 +0600

    18.2

---
+++ array.js
@@ -0,0 +1,7 @@
+var friendsAge = [11, 24, 54, 52, 7];
+var picnicFee = [5000, 4000, 2000, 3000];
+var naykas = ['mahi', 'opu', 'sabnoor', 'sabana'];
+var vowels = ['a', 'e', 'i', 'o', 'u'];
+
+console.log(vowels);
+console.log(naykas.length);
