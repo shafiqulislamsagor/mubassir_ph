5d789974ff7ba6cacd130d4aecbdb46f2628db09
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-12-14 13:50:39 +0600

    18.3

---
+++ index.js
@@ -0,0 +1,15 @@
+var numbers = [45, 68, 78, 56, 89, 98];
+
+// get Element by index
+console.log(numbers[3]);
+
+var element = numbers[1];
+console.log(element);
+
+// get Element value by index
+numbers[1] = 77;
+console.log(numbers);
+
+// find index of an Element
+var positinIndex = numbers.indexOf(89);
+console.log(positinIndex);
