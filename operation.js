d92f94a5eca177d63d2a2b0db5d960f618ae422d
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-12-14 06:00:26 +0600

    17.6

---
+++ operation.js
@@ -0,0 +1,8 @@
+var onionPrice = 20 ;
+var eggPrice = 10 ;
+var totalPrice = onionPrice + eggPrice ;
+
+console.log(onionPrice);
+console.log(eggPrice);
+console.log(onionPrice + eggPrice);
+console.log(totalPrice);
