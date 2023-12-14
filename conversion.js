b652ca6b1bec06c0ad05f625915387015161301f
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-12-14 06:34:06 +0600

    17.8

---
+++ conversion.js
@@ -0,0 +1,8 @@
+var firstName = "Shafiqul";
+var lastName = "Islam";
+var fullName = firstName + " " + lastName;
+console.log(fullName)
+var price1 = '21';
+var price2 = 20;
+var total = price2 + parseInt(price1) + price2;
+console.log(total);
