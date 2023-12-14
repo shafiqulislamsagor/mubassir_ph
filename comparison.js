a95393e27d1cc5da86e031cd2f9107b0df9c617d
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-12-14 17:03:23 +0600

    18.5

---
+++ comparison.js
@@ -0,0 +1,20 @@
+console.log(5 < 6);
+console.log(5 > 6);
+console.log(5 == 6);
+console.log(6 == 6);
+console.log(5 != 6);
+console.log(5 != 5);
+console.log(5 <= 6); // less than or equal
+console.log( 6 <= 6);
+console.log( 16 <= 6);
+console.log(5 >= 6);
+console.log( 5 >= 2);
+console.log(2 >= 2);
+
+
+var num1 = 56;
+var num2 = 89;
+
+console.log(num1 !== num2);
+
+
