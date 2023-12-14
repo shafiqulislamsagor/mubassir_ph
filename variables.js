71b4c3e460ce76779a2f1dec93346bdd3153717a
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-12-14 12:22:10 +0600

    18.1k

---
+++ variables.js
@@ -0,0 +1,29 @@
+// number type variable
+var sunGlassPrice = 120;
+// string type variable
+var specialOneName = "Shafiqul Islam Sagor";
+//boolean type variable
+var isSingle = true;
+
+// two type of number : Integer : 120,
+// Float , decimal:3.65,
+
+// variable naming convention : rules, best prectice
+/**
+ * 1 Single word
+ * 2 no gap or speace 
+ * 3 no quote
+ * 4 no keyword
+ * 5 con not start with a number
+ * 6 can not contain dash
+ * 7 should use camelCase
+ */
+
+//Matth operations:+,-,*,/
+// Shorthand:+= , -=,++,--
+
+//Convert: parseInt, parseFloat
+
+//toFixed
+
+//remainder : vagses: %
