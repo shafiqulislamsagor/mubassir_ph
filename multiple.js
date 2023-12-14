29cf3f0b11365801026e129b95c9c54c75116a98
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-12-14 22:10:08 +0600

    18.7

---
+++ multiple.js
@@ -0,0 +1,22 @@
+var isGraduated = false;
+var salary = 60000;
+var cars = 1;
+
+if (isGraduated == true){
+    console.log('Eso biye kore feli')
+}
+else {
+    console.log('tor kopale biye nai')
+}
+if (isGraduated == true || salary > 50000){
+    console.log('Eso biye kore feli')
+}
+else {
+    console.log('tor kopale biye nai')
+}
+if (isGraduated == true && salary > 50000 || cars >= 1){
+    console.log('Eso biye kore feli')
+}
+else {
+    console.log('tor kopale biye nai')
+}
