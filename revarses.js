3efc8d097f687ba6f759868acd02f7b725bd978b
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-12-16 01:19:57 +0600

    19.7

---
+++ revarses.js
@@ -0,0 +1,20 @@
+/* for ( var num = 0; num <= 20; num++){
+    console.log(num);
+}
+for (var n = 20 ; n >= 0 ; n--){
+    console.log(n)
+} */
+var namber = 50;
+
+/* while(namber <= 50){
+    console.log(namber);
+    namber--;
+    if(namber < 0){
+        break;
+    }
+} */
+
+while(namber >= 0){
+    console.log(namber);
+    namber--;
+}
