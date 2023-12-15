3fe73d92b07f7e02f0e28a2ab5b96a97a6bd5bee
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-12-15 18:54:32 +0600

    while loop

---
+++ while.js
@@ -0,0 +1,8 @@
+var roastGiven = 0;
+while(roastGiven <= 7){
+    console.log('Roast Den, please!!')
+    //roastGiven = roastGiven + 1;
+    // roastGiven += 1;
+    roastGiven ++;
+    console.log(roastGiven)
+}
