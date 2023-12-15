9af7c37bbaa59a894bf21cd56373694ff526dd8e
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-12-16 02:55:15 +0600

    20.1

--- jaan.js
+++ jaan.js
@@ -1,2 +1,10 @@
 console.log(7);
 console.log(3);
+var number = [1,2,3,4,5,6,7,8,9]
+number = number.slice(2,7);
+console.log(number)
+
+var string = 'Shafiqul islam is a good boy';
+
+string = string.includes('has');
+console.log(string)
