014635a5187e0fe618da2f222ade8918371a5945
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-12-15 19:16:33 +0600

    19.3

---
+++ even.js
@@ -0,0 +1,7 @@
+// target : 1 to 40 all event numbers
+
+var number = 0;
+while(number <= 10){
+    console.log(number);
+    number += 2;
+}
--- while.js
+++ while.js
@@ -1,3 +1,9 @@
+// 1. Loop variable
+// 2. condition inside while
+// 3. Loop body 
+// 4. Change the Loop variable
+
+
 var roastGiven = 0;
 while(roastGiven <= 7){
     console.log('Roast Den, please!!')
