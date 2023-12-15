be37da0cc6cb185a9daf995e308eb1f6bf5acb63
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-12-15 20:44:24 +0600

    19.5

---
+++ array-loop.js
@@ -0,0 +1,13 @@
+var numbers = [45, 87, 89, 56, 32, 51, 25];
+
+for(var i = 0 ; i < numbers.length ; i++){
+    var number = numbers[i];
+    console.log(number)
+}
+
+var items = ['book', 'pen', 'headphn', 'phn', 'mic', 'mouse'];
+
+for (var i = 0; i < items.length; i++){
+    var item = items[i];
+    console.log(item);
+}
