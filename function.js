0486e6325ce194e3d40a2968f6acd205f033e50c
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-12-16 13:40:41 +0600

    20.2

---
+++ function.js
@@ -0,0 +1,14 @@
+function kita(){
+    console.log('Shafiqul Islam Sagor');
+    console.log('Tor name ki re?')
+}
+
+kita();
+
+function namber(point){
+    console.log(point);
+    console.log(point + point);
+}
+
+namber(100);
+namber(1020);
