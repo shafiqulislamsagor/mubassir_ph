de56e102718799b993e8ef33db5cea9986250385
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-12-17 19:08:04 +0600

    20.3

---
+++ parameter.js
@@ -0,0 +1,14 @@
+function bring(money){
+    console.log('taka disen',money);
+    console.log('ai nen singara');
+}
+
+bring(250);
+
+function sum (a , b, c, d, e){
+    console.log(a, b, c, d, e);
+    var sum = a + b + c + d + e;
+    console.log(sum);
+}
+
+sum(1,2,5,8,9)
