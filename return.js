ce7dc4279d8835d50081e8bc2c2cf32556731afa
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-12-18 11:34:39 +0600

    20.4

---
+++ return.js
@@ -0,0 +1,23 @@
+function sum(sum1,sum2){
+    console.log(sum1,sum2);
+    var sum = sum1 + sum2;
+    console.log(sum)
+    return sum;
+}
+
+sum(20,23);
+
+console.log(sum(20,20))
+
+function singara(money){
+    console.log("My money : ", money);
+    var price = 10 ;
+    console.log('Singara price :', price);
+    var count = money/ price;
+    return count;
+}
+
+
+var Taka = 1520;
+var countSingara = singara(Taka);
+console.log('Eating singaras: ',countSingara);
