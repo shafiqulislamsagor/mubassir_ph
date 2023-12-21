f9505fb4f6d807c787dc30799c379827bff63887
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-12-21 15:49:46 +0600

    some

--- return.js
+++ return.js
@@ -8,6 +8,7 @@ function sum(sum1,sum2){
 sum(20,23);
 
 console.log(sum(20,20))
+console.log("kisu din rest nitasi.. tumi kunda kow")
 
 function singara(money){
     console.log("My money : ", money);
