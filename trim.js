fc8cbda1ce162edf478c3e8b960218983218abb8
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2023-12-24 17:42:43 +0600

    trim

---
+++ trim.js
@@ -0,0 +1,9 @@
+let text1 = "   Shafiqul   Islam     Sagor    ";
+let text2 = text1.trimStart();
+let text3 = text1.trim();
+let text4 = text1.trimEnd();
+console.log(text2);
+console.log(text1.length);
+console.log(text2.length);
+console.log(text3.length);
+console.log(text4.length);
